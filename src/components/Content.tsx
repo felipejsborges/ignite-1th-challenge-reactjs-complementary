5c0a22bbc78c9b20666e09bb46b5b7867c8e7756
Felipe Borges <felipejsborges@outlook.com>
2021-03-20 03:47:13 -0300

    challenge completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,26 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+import { MovieProps, GenreResponseProps } from '../App'
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
+  const { selectedGenre, movies} = props
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
